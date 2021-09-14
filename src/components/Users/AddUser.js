be23c57c33163c53fe5ef50806b164ef51a1ebc0
Yaghoob Abdollahi <yaghoob.silvana14@gmail.com>
2021-09-14 16:44:14 +0430

    add styles to AddUser component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,22 @@
+import Card from "../UI/Card";
+import classes from './AddUser.module.css'
 
-const AddUser = props => {
-    const adduserHandler = event => {
-        event.preventDefault();
-    }
+const AddUser = (props) => {
+  const adduserHandler = (event) => {
+    event.preventDefault();
+  };
 
-    return (
-        <form onSubmit={adduserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input id='username' type='text'></input>
-            <label htmlFor='age'>Age (Years)</label>
-            <input id='age' type='number'></input>
-            <button type='submit'>Add User</button>
-        </form>
-    )
-}
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={adduserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
 
-export default AddUser
+export default AddUser;
