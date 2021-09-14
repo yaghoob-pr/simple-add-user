676a5cb83c3ceb6ac59820b1161e471a6855f587
Yaghoob Abdollahi <yaghoob.silvana14@gmail.com>
2021-09-14 17:01:47 +0430

    add custom button component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,3 +1,4 @@
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from './AddUser.module.css'
 
@@ -13,7 +14,7 @@ const AddUser = (props) => {
         <input id="username" type="text"></input>
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
+        <Button type='submit'>Add User</Button>
       </form>
     </Card>
   );
