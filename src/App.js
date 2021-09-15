6c3c8a8765fcee080368c8b9b680d1eb434330a0
Yaghoob Abdollahi <yaghoob.silvana14@gmail.com>
2021-09-15 15:14:39 +0430

    add userList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import {useState} from 'react'
+
 import AddUser from './components/Users/AddUser';
 import './App.css';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [userList, setUsersList] = useState([])
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevState) => {
+      return [...prevState, {name: uName, age: uAge, id: Math.random().toString()}]
+    })
+  }
   return (
     <div>
-      <AddUser/>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={userList}/> 
     </div>
   );
 }
